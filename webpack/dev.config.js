21c48fb070bdbc253d0b50121d9380469bfa8d29
jcchen <junchangchen@gmail.com>
2016-03-21 17:38:37 +0800

    init

---
+++ webpack/dev.config.js
@@ -0,0 +1,35 @@
+
+var fs = require('fs');
+var path = require('path');
+
+var root = path.join(__dirname, "/..")
+
+var babelrc = fs.readFileSync(path.join(root, '/.babelrc'));
+var babelrcObj = {};
+
+try {
+    babelrcObj = JSON.parse(babelrc);
+} catch (err) {
+    console.log('==>    ERROR: Error parsing your .babelrc.');
+    console.log(err)
+}
+
+
+module.exports = {
+    
+    context: root,
+    
+    entry: "./src/js/index.js",
+
+    output: {
+        path: "./src/js/",
+        filename: "bundle.js"
+    },
+
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/, query: {'presets': babelrcObj.presets} }
+        ]
+    }
+};
